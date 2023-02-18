fa32f99ee78f00a87419fca0254ba747fdb07789
rohith42 <36945963+rohith42@users.noreply.github.com>
2023-02-18 01:54:40 -0800

    got a barebones app functional

--- App.js
+++ App.js
@@ -1,12 +1,61 @@
+import { useContext } from 'react';
+import { StyleSheet, Text } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+
+import LoginScreen from './screens/LoginScreen';
+import SignupScreen from './screens/SignupScreen';
+import AppContext, { AppContextProvider } from './store/AppContext';
+
+const Stack = createNativeStackNavigator();
+// Stack to authenticate users (login/signup)
+function AuthStack() {
+  return (
+    <Stack.Navigator>
+      <Stack.Screen 
+        name="LoginScreen" 
+        component={LoginScreen}
+        options={{ title:'Log in' }}
+      />
+      <Stack.Screen 
+        name="SignupScreen" 
+        component={SignupScreen}
+        options={{ title:'Sign up' }} 
+      />
+    </Stack.Navigator>
+  );
+}
+
+const Tab = createBottomTabNavigator();
+// Stack for autheticated users
+function AuthenticatedStack() {
+  return (
+    <Tab.Navigator>
+    </Tab.Navigator>
+  );
+}
+
+function Navigation() {
+  const { cookie } = useContext(AppContext);
+
+  return (
+    <NavigationContainer>
+      {!cookie && <AuthStack/>}
+      {!!cookie && <AuthenticatedStack/>}
+    </NavigationContainer>
+  );
+}
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <>
+      <StatusBar style="dark" />
+      <AppContextProvider>
+        <Navigation />
+      </AppContextProvider>
+    </>
   );
 }
 
