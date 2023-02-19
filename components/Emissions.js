12bb3a6a0d3af820d8c5e68fbc26997b63c5c9a0
rohith42 <36945963+rohith42@users.noreply.github.com>
2023-02-19 05:05:38 -0800

    Started working on emissions

--- components/Emissions.js
+++ components/Emissions.js
@@ -3,20 +3,74 @@ import {
   View,
   StyleSheet,
   Text,
-  SafeAreaView
+  SafeAreaView,
+  ScrollView
 } from 'react-native';
-import { Checkbox, Button } from 'react-native-paper';
+
 import AppContext from '../store/AppContext';
+import { ProgressChart } from 'react-native-chart-kit';
+import { COLORS } from '../styles/Colors';
+import TripItem from './TripItem';
 
 export default function Emissions({ navigation }) {
   const { setCookie } = useContext(AppContext);
+// How many kg carbon saved
+const [progress, setProgress] = useState(5.0);
+// carbon saved goal
+const goal = 20.0;
 
-  
-  return (
-    <View style={styles.inner}>
-      <Text>This is the Emissions component</Text>
+
+
+return (
+  <View style={styles.inner}>
+    <View style={styles.chartCard}>
+      <View>
+        <Text style={styles.chartTitle}>Uber</Text>
+        <ProgressChart 
+          data={[1, 1, 1, (progress/goal)]}
+          width={200} height={200} hideLegend
+          chartConfig={{
+            backgroundGradientFrom: COLORS.lightGray,
+            backgroundGradientTo: COLORS.lightGray,
+            color: (opacity=1, index) => (
+              index === 3 ? `rgba(102, 102, 102, ${opacity})`:
+              'rgba(232, 232, 232, 1)'
+            )
+          }}
+        />
+      </View>
+      <View style={styles.textContainer}>
+        <Text style={styles.greenText}>
+          {`${progress} kg CO2e`}
+        </Text>
+        <Text style={styles.grayText}>
+          {`${goal-progress} kg remaining`}
+        </Text>
+      </View>
     </View>
-  );
+
+    <Text style={styles.pastTrips}>Past trips</Text>
+
+    <ScrollView style={styles.container}
+      contentContainerStyle={{width:'100%'}}
+      directionalLockEnabled
+    >
+      {dummyData.map((o, i, a) => (
+        <TripItem
+          color={COLORS.green}
+          carbon={o.carbon}
+          date={o.date}
+          time={o.time}
+          type={o.type}
+          last={i === a.length-1}
+          key={i}
+        />
+      ))}
+    </ScrollView>
+    
+    
+  </View>
+);
 }
 
 const styles = StyleSheet.create({
