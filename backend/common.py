8e1141138a2afe93884d53124572ae76b17bf176
rohith42 <36945963+rohith42@users.noreply.github.com>
2023-03-03 17:36:09 -0800

    Update common.py

--- backend/common.py
+++ backend/common.py
@@ -1,3 +1,3 @@
 DATABASE_ENGINE_STRING = "sqlite:///test.db"
-JWT_SECRET = "SVpq/BjTm4EcNupviSpepxUpEuXEwpIKE0WTqURRcLI="
+JWT_SECRET = "Vpq/BjTm4EcNupviSpepxUpEuXEwpIKE0WTqURRcLI" # Missing Several characters
 JWT_ENCODE_ALGO = "HS256"
